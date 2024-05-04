696140f42b6936d7a2c67940fc1f2fd9e8821633
Yamizaki <Arakaki_18@outlook.com>
2024-05-03 22:59:59 -0500

    first commit

---
+++ ejecutable.py
@@ -0,0 +1,208 @@
+from selenium.webdriver import Chrome
+
+from selenium import webdriver
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.chrome.service import Service
+
+import time
+
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.webdriver.common.by import By
+
+from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.support import select
+
+from datetime import datetime
+
+import os
+
+
+#Config
+with open("config.txt", "r") as archivo:
+    # Lee todas las líneas del archivo
+    lineas = archivo.readlines()
+
+# Inicializa un diccionario para almacenar las constantes
+constantes = {}
+
+# Itera sobre las líneas del archivo
+for linea in lineas:
+    # Divide cada línea en nombre de la constante y su valor
+    nombre, valor = linea.strip().split("=")
+    # Elimina espacios en blanco alrededor del nombre y del valor
+    nombre = nombre.strip()
+    valor = valor.strip()
+    # Almacena la constante en el diccionario
+    constantes[nombre] = eval(valor)
+    
+    
+# ENV VAR
+USER = constantes["Usuario"]
+PASSWORD = constantes["Contrasena"]
+
+NAME_LIST = constantes["NombreArchivo"]
+PATH_FILE = constantes["UbicacionArchivo"]
+
+DOWNLOAD_PATH = constantes["UbicacionDescargaArchivo"]
+
+def main():
+    service = Service(ChromeDriverManager().install())
+    option = webdriver.ChromeOptions()
+    option.add_argument("--window-size=1920,1080")
+    # option.add_argument("--headless")
+
+
+# Init serv
+    driver = Chrome(service=service, options=option)
+    driver.get("https://mailerball.com/")
+    
+    
+# Our logic goes here
+    time.sleep(3)
+    
+    # Looking for login and click
+    login = driver.find_element(By.XPATH, "/html/body/div[1]/nav/div/ul/li[3]/a/span").click()
+    time.sleep(3)
+    
+    
+    #Modifying session
+    user = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/form/div[2]/div/div[1]/div[2]/input").send_keys(USER)
+    time.sleep(1)
+    
+    pwd = driver.find_element(By.ID, "user_password").send_keys(PASSWORD)
+    time.sleep(1)
+
+    #Acces
+    acc_btn = driver.find_element(By.XPATH, "//*[@id='new_user']/div[2]/div/div[1]/div[5]/input").click()
+    print("Login...")
+    time.sleep(3)
+    
+    
+    #Menu Acc
+    menu = driver.find_element(By.XPATH, "//*[@id='sidenav']/i").click()
+    time.sleep(1)
+    
+    # Acces to list
+    menu_list = driver.find_element(By.XPATH, "//*[@id='slide-out']/li[2]/a").click()
+    time.sleep(1)
+    
+    
+    # Add button
+    add_btn = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/a/i").click()
+    print("Iniciando procesador de archivo CSV...")
+    time.sleep(2)
+    
+    
+    # Type Name
+    name_list = driver.find_element(By.XPATH, "//*[@id='lista_nombre']").send_keys(NAME_LIST)
+    time.sleep(1)
+    
+    #Upload file CSV
+    file_to_up = driver.find_element(By.XPATH, "//*[@id='archivo']").send_keys(PATH_FILE)
+    
+    
+    # Select Encode
+    encode_opt = driver.find_element(By.XPATH, "//*[@id='new_lista']/div[2]/div[2]/div/div/input")
+    encode_opt.click()
+    encode_opt.send_keys(Keys.ARROW_DOWN)
+    encode_opt.send_keys(Keys.ENTER)
+    time.sleep(1)
+   
+   
+   
+   #Submit 
+    encode_opt = driver.find_element(By.XPATH, "//*[@id='new_lista']/div[3]/div/input").click()
+    print("Enviando datos para generar CSV...")
+    time.sleep(5)
+   
+
+    #Menu Acc
+    menu = driver.find_element(By.XPATH, "//*[@id='sidenav']/i").click()
+    time.sleep(1)
+    
+    # Acces to list
+    menu_list = driver.find_element(By.XPATH, "//*[@id='slide-out']/li[2]/a").click()
+    time.sleep(1)
+    
+    
+    # Procces to download csv file
+    condition = True 
+    
+    while condition:
+        
+        table_dest = driver.find_element(By.XPATH, "/html/body/div[2]/div/table/tbody/tr[1]/td[3]/a")
+        texto = table_dest.text
+        txt_split = texto.split()
+        print("Esperando se actualice los destinatarios...")
+        
+        if "0" not in txt_split:
+            condition = False
+            table_dest.click()
+            print("Destinatarios Listo...")
+            time.sleep(5)
+        else:
+            time.sleep(30)
+            
+            #Menu Acc
+            menu = driver.find_element(By.XPATH, "//*[@id='sidenav']/i").click()
+            time.sleep(1)
+            
+            # Acces to list
+            menu_list = driver.find_element(By.XPATH, "//*[@id='slide-out']/li[2]/a").click()
+            time.sleep(1)
+        
+    
+    
+    #Download csv
+    table_dest = driver.find_element(By.XPATH, "/html/body/div[2]/div/ul[1]/li[2]/a").click()
+    print("Descargando archivo csv...")
+    time.sleep(10)
+    
+    # To close navigator
+    driver.quit()
+    
+    
+    
+def moveFile():
+    #Move downloaded file
+    path_file_downloaded = os.path.expanduser("~\Downloads")
+    
+    
+    
+    file_name = f"list_{NAME_LIST}__.xls"
+    
+    while True:
+        if file_name in os.listdir(path_file_downloaded):
+            break
+            print("...")
+        else:
+            print(file_name)
+            print(path_file_downloaded)
+            print(os.listdir(path_file_downloaded))
+            time.sleep(10)
+            
+    ruta_archivo = os.path.join(path_file_downloaded, file_name)
+    print(ruta_archivo)
+    ruta_destino =os.path.join(DOWNLOAD_PATH, file_name)
+    print(ruta_destino)
+    
+    if os.path.exists(ruta_archivo):
+        print("ruta download existe")
+    else:
+        print("download no existe")
+        
+    if os.path.exists(ruta_destino):
+        print("ruta download existe2")
+    else:
+        print("download no existe2")
+        
+    os.rename(ruta_archivo, ruta_destino)
+
+
+if __name__ == '__main__':
+    main()
+    moveFile()
